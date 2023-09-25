2a48d9699c9c4cde2a2a5105197109ac970efbbc
Kai Nguyen <kai.nguyen@aligent.com.au>
2023-09-25 13:34:40 +0930

    DO-1499: throw error if do not provide credentials

--- pipe/entrypoint.ts
+++ pipe/entrypoint.ts
@@ -6,18 +6,22 @@ import { uploadDeploymentBadge } from "./uploadDeploymentBadge";
 
 findServerlessYaml(`${process.env.BITBUCKET_CLONE_DIR}/services`)
   .then((files) =>
-    Promise.all(files.map((file) => injectCfnRole(file, process.env.CFN_ROLE)))
+    Promise.all(files.map((file) => injectCfnRole(file, env.cfnRole)))
   )
-  .then(() =>
+  .then(() => {
+    if (!env.awsAccessKeyId || !env.awsSecretAccessKey) {
+      throw new Error("AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY not set");
+    }
+
     runCLICommand([
       "npm ci",
-      `npx serverless config credentials --provider aws --profile ${env.profile} --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY}`,
+      `npx serverless config credentials --provider aws --profile ${env.profile} --key ${env.awsAccessKeyId} --secret ${env.awsSecretAccessKey}`,
       `npx nx run-many -t deploy --stage ${env.stage} --aws-profile ${env.profile}`,
-    ])
-  )
+    ]);
+  })
   .then(() => uploadDeploymentBadge(true))
-  .catch(() => {
-    console.error(`Something went wrong!`);
+  .catch((error) => {
+    console.error(error);
     uploadDeploymentBadge(false);
   });
 
